10d7b34b2eea6aa6a88fe886ffc032c997498e06
Rajat Khatri <rajatkhatri0002@gmail.com>
2021-08-05 02:49:12 +0530

    added the app to the setting for rendering

--- Blog_Project/blog/views.py
+++ Blog_Project/blog/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    
+    
+    {
 
+            'author':'Kush',
+            'title':'Blog Post 1',
+            'content':'First post content',
+            'date_posted':'August 5 , 2021'
+    
+},
+
+{
+
+            'author':'Kush',
+            'title':'Blog Post 2',
+            'content':'second post content',
+            'date_posted':'August 5 ,2021'
+
+
+}
+
+]
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+
+            'posts':posts,
+            'title': 'Home'
+
+    }
+    return render(request, 'blog/home.html' , context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
